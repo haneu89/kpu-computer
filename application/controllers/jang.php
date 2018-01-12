29e4b20c55b7bb074a7a3ae75dbfeea730314b66
joju0 <dnelths@naver.com>
2018-01-12 14:09:11 +0900

    ㅇㅇ

--- application/controllers/jang.php
+++ application/controllers/jang.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class jang extends CI_Controller {
     public function index() {
-        echo "안녕하십니까 안녕않녕";
+        echo "안녕하십니까 안녕않녕 짱형바보";
         // $this->load->view('kpu');
     }
 }
