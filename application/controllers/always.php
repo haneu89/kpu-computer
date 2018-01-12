de363d27b623ae37656c536c9dab64e922fd92bf
unknown <always_walk@naver.com>
2018-01-12 13:39:43 +0900

    always 컨트롤러 추가

---
+++ application/controllers/always.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class always extends CI_Controller {
+    public function index() {
+        echo "안녕하십니까 안녕않녕";
+        // $this->load->view('kpu');
+    }
+}
+
+?>
