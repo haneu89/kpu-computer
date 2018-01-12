01e8d062795db023a3957d97c86fcdd9e9b2b643
YeomJunghee <yyeom1021@gmail.com>
2018-01-12 14:03:35 +0900

    Yeom midify

--- application/controllers/Jung.php
+++ application/controllers/Jung.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Yeom extends CI_Controller {
+class Jung extends CI_Controller {
     public function index() {
         echo "안녕하십니까 염정희 입니다.";
     }
