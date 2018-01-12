265ec95da4dfdb908fc75239a038f9cf8261b982
unknown <wldbs3447@naver.com>
2018-01-12 14:50:32 +0900

    변경 저장

--- application/controllers/jiyoon.php
+++ application/controllers/jiyoon.php
@@ -1,11 +1,21 @@
-<?php
+//?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
-class jiyoon extends CI_Controller {
+/*class Jiyoon extends CI_Controller {
 
     public function index(){
        echo "안녕하세요 박지윤입니다."."<br>";
     }
     }
+?>*/
 
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$message_id = $_GET['message'];
+echo "메시지"."<br>";
 ?>
+
+<form action="post.php" method="POST">
+    <input type="text" name="message">
+    <button>전송</button>
+</form>
